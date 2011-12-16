a2e45741d7e2ed82dffc88b6e2512655106f35a7
Karel Minarik <karmi@karmi.cz>
2011-12-16 10:28:33 +0100

    Added an example of how to intercept exceptions due to incorrect user queries
    When the users enters an incorrect query, _Tire_ raises a `Tire::Search::SearchRequestFailed` exception.
    
    It is unfortunate to propagate the raw exception to the page, and display the “Something went wrong” page
    in this case -- it's better to display the default result set, or the search form, and indicate to the
    user her query was incorrect.
    
    This commit contains a trivial implementation via the _Rails'_ `rescue_from` method.
    
    See <http://github.com/karmi/tire/issues/164#issuecomment-3096189>.

--- episode-307/blog-after/app/controllers/articles_controller.rb
+++ episode-307/blog-after/app/controllers/articles_controller.rb
@@ -1,4 +1,14 @@
 class ArticlesController < ApplicationController
+  rescue_from Tire::Search::SearchRequestFailed do |error|
+    # Indicate incorrect query to the user
+    if error.message =~ /SearchParseException/ && params[:query]
+      flash[:error] = "Sorry, your query '#{params[:query]}' is invalid..."
+    else
+      # ... handle other possible situations ...
+    end
+    redirect_to root_url
+  end
+
   def index
     @articles = Article.search(params)
   end
